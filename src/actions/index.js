3904ed1c4bd683c9783f69851ce92e31da8f3621
Egor-Ivankov <122007814+Egor-Ivankov@users.noreply.github.com>
2023-08-21 15:39:59 +0300

    It remains to complete the filters

---
+++ src/actions/index.js
@@ -0,0 +1,32 @@
+export const heroesFetching = () => {
+    return {
+        type: 'HEROES_FETCHING'
+    }
+}
+
+export const heroesFetched = (heroes) => {
+    return {
+        type: 'HEROES_FETCHED',
+        payload: heroes
+    }
+}
+
+export const heroesFetchingError = () => {
+    return {
+        type: 'HEROES_FETCHING_ERROR'
+    }
+}
+
+export const heroesDelete = (id) => {
+    return {
+        type: 'HERO_DELETE',
+        payload: id
+    }
+}
+
+export const heroesAdd = (newHero) => {
+    return {
+        type: 'HERO_ADD',
+        payload: newHero
+    }
+}
---
+++ src/components/heroesAddForm/HeroesAddForm.js
@@ -0,0 +1,94 @@
+import { useState } from "react";
+import { useDispatch } from "react-redux";
+import { heroesFetching, heroesAdd } from "../../actions";
+import { useHttp } from "../../hooks/http.hook";
+import { uid } from "uid";
+
+// Задача для этого компонента:
+// Реализовать создание нового героя с введенными данными. Он должен попадать
+// в общее состояние и отображаться в списке + фильт    роваться
+// Уникальный идентификатор персонажа можно сгенерировать через uiid
+// Усложненная задача:
+// Персонаж создается и в файле json при помощи метода POST
+// Дополнительно:
+// Элементы <option></option> желательно сформировать на базе
+// данных из фильтров
+
+const HeroesAddForm = () => {
+    const [name, setName] = useState('');
+    const [description, setDescription] = useState('');
+    const [element, setElement] = useState('');
+    const dispatch = useDispatch();
+
+    const newHero = {
+        id: uid(),
+        name,
+        description,
+        element
+    }
+
+    const {request} = useHttp();
+    
+    const onHandleSubmit = (e) => {
+        e.preventDefault();
+        dispatch(heroesFetching());
+        request('http://localhost:3001/heroes', "POST", JSON.stringify(newHero))
+            .then(dispatch(heroesAdd(newHero)))
+            .catch(err => console.log(err))
+    }
+
+    return (
+        <form className="border p-4 shadow-lg rounded" onSubmit={onHandleSubmit}>
+            <div className="mb-3">
+                <label htmlFor="name" className="form-label fs-4">Имя нового героя</label>
+                <input 
+                    required
+                    type="text" 
+                    name="name" 
+                    className="form-control" 
+                    id="name" 
+                    placeholder="Как меня зовут?"
+                    value={name}
+                    onChange={(e) => setName(e.target.value)}
+                    />
+            </div>
+
+            <div className="mb-3">
+                <label htmlFor="text" className="form-label fs-4">Описание</label>
+                <textarea
+                    required
+                    name="text" 
+                    className="form-control" 
+                    id="text" 
+                    placeholder="Что я умею?"
+                    style={{"height": '130px'}}
+                    value={description}
+                    onChange={(e) => setDescription(e.target.value)}
+                    />
+                    
+            </div>
+
+            <div className="mb-3">
+                <label htmlFor="element" className="form-label">Выбрать элемент героя</label>
+                <select 
+                    required
+                    className="form-select" 
+                    id="element" 
+                    name="element"
+                    value={element}
+                    onChange={(e) => setElement(e.target.value)}
+                    >
+                    <option >Я владею элементом...</option>
+                    <option value="fire">Огонь</option>
+                    <option value="water">Вода</option>
+                    <option value="wind">Ветер</option>
+                    <option value="earth">Земля</option>
+                </select>
+            </div>
+
+            <button type="submit" className="btn btn-primary">Создать</button>
+        </form>
+    )
+}
+
+export default HeroesAddForm;
